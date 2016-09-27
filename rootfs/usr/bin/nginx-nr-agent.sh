6e738b9278fca2627b5d109c2d0afac03af0735f
Evgeny Dudin <nagliyvred@gmail.com>
2016-09-27 17:45:07 +1000

    sed'ing nginx-nr-agent.ini properly

--- rootfs/usr/bin/nginx-nr-agent.sh
+++ rootfs/usr/bin/nginx-nr-agent.sh
@@ -2,7 +2,8 @@
 
 mkdir -p /var/run/nginx-nr-agent && chown nobody /var/run/nginx-nr-agent
 if [ "$NEW_RELIC_LICENSE_KEY" != "" ]; then
-    sed "s/NEW_RELIC_APP_NAME/$NEW_RELIC_APP_NAME/g; s/NEW_RELIC_LICENSE_KEY/$NEW_RELIC_LICENSE_KEY/g" /etc/nginx-nr-agent/nginx-nr-agent.ini > /dev/null
+    sed "s/NEW_RELIC_APP_NAME/$NEW_RELIC_APP_NAME/g; s/NEW_RELIC_LICENSE_KEY/$NEW_RELIC_LICENSE_KEY/g" \
+        /etc/nginx-nr-agent/nginx-nr-agent.ini.template > /etc/nginx-nr-agent/nginx-nr-agent.ini
     echo "Starting Nginx Newrelic Agent"
     exec /usr/bin/nginx-nr-agent.py -c /etc/nginx-nr-agent/nginx-nr-agent.ini -f start > /var/log/nginx-nr-agent.log
 else
